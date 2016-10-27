09fa40034d6990d0ee962fa2c1b56dee1cd5b363
Durkin <durk7832@cs-wormulon.cs.uidaho.edu>
2016-10-27 14:38:46 -0700

    fixing comments

--- cs120/Lab8/Ex1.1.c
+++ cs120/Lab8/Ex1.1.c
@@ -22,8 +22,8 @@ int main(){
 	clear(); // clears the window
 	refresh(); // reprints the window
 	Setup(room,x,y); // setup the room with the robot
-	find(room);
-//	print(room); // write this function!!
+//	print(room); // use this function for first exercise
+	find(room); // use this function for second exercise
 	refresh();
 	WaitForQuit();
 	endwin(); // frees the screen for normal use
