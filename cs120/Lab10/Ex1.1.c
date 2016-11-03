c515e2cd9a0dd86aa703bced07b51aa8aa1ed3b5
Durkin <durk7832@cs-wormulon.cs.uidaho.edu>
2016-11-03 15:00:40 -0700

    editing

--- cs120/Lab10/Ex1.1.c
+++ cs120/Lab10/Ex1.1.c
@@ -1,59 +1,62 @@
 /* Zane Durkin
  * Lab10
- * November 2
+ * November 3 2016
  */
 
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-bool isData(string line){
-	bool data = false;
-	for(int i=0; i < line.size(); i++){
-		if(line[i] == 'A' || line[i] == 'C' || line[i] == 'G' || line[i] == 'T'){
-			data = true;
-			break;
-		}else if(line[i] == '-' || line[i] == '|'){
-			break;
+string complementBase(char a){
+	// find the given letter and return the correct string
+	switch(a){
+			case 'A':
+				return "T   ";
+				break;
+			case 'C':
+				return "G   ";
+				break;
+			case 'G':
+				return "C   ";
+				break;
+			case 'T':
+				return "A   ";
+				break;
 		}
-	}
-	return data;
 }
 
 void DrawDNA(string line){
+	// start with buffer spaces
 	string response1 = "  ";
 	string response2 = "  ";
 	string response3 = "  ";
 	string response4 = "-"; 
+	// for every character, get the compelemnt and build the footer
 	for(int i = 0; i < line.size(); i++){
-		switch(line[i]){
-			case 'A':
-				response1 += ".   ";
-				response2 += "T   ";
-				response3 += "|   ";
-				response4 += "-+--";
-				break;
-			case 'C':
-				response1 += ".   ";
-				response2 += "G   ";
-				response3 += "|   ";
-				response4 += "-+--";
-				break;
-			case 'G':
-				response1 += ".   ";
-				response2 += "C   ";
-				response3 += "|   ";
-				response4 += "-+--";
-				break;
-			case 'T':
-				response1 += ".   ";
-				response2 += "A   ";
-				response3 += "|   ";
-				response4 += "-+--";
-				break;
-		}
+			response1 += ".   ";
+			response2 += complementBase(line[i]);
+			response3 += "|   ";
+			response4 += "-+--";
 	}
+	// output everything
 	cout << response1 << endl << response2 << endl << response3 << endl << response4 << endl;
+}
+
+void DrawHeader(string str){
+	// get the size of the given string
+	int count = str.size();
+	string response1 = "-";
+	string response2 = "  ";
+	string response3 = "  ";
+	// for each letter, print a set of header stuff
+	for(int i = 0; i<count; i++){
+		response1 += "-+--";
+		response2 += "|   ";
+		response3 += str[i];
+		response3 += "   ";
+	}
+	// output everything
+	cout << response1 << endl << response2 << endl << response3 << endl;
 }
 
 void prefix(int count, string list, string& prefix){
@@ -63,16 +66,19 @@ void prefix(int count, string list, string& prefix){
 }
 
 int main(){
+	// get file
 	char fileName[40];
 	fstream file;
 	cout << "Enter File Name:" << endl;
 	cin >> fileName;
 	file.open(fileName);
 	string line;
-	while(getline(file, line)){
-		cout << line << endl;
-		if(isData(line)){
-			DrawDNA(line);
-		}
-	}
+	// draw dna
+	getline(file, line);
+	DrawHeader(line);
+	DrawDNA(line);
+	// get prefix of line
+	string pre;
+	prefix(4, line, pre);
+	cout << "Prefix is: " << pre << endl;
 }
