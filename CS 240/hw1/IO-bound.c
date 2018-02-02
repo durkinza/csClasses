c0540db110f5ebc7f472c615b0d16cdd7c3f6efe
Zane Durkin <zanedurkin@gmail.com>
2018-02-01 19:35:03 -0800

    updating hw1

--- CS 240/hw1/IO-bound.c
+++ CS 240/hw1/IO-bound.c
@@ -3,13 +3,10 @@
 #include <stdlib.h>
 
 int main(){
-
-	int x;
 	clock_t init, final;
 	FILE *fp;
 	init=time(NULL);	
 	final = init + (5 * 60);
-	
 	while(time(NULL) < final ){
 		fp=fopen("IO-output.txt", "w");
 		fseek(fp, 0, SEEK_SET);
