056b8f825f45ab8bda89aa85a4e288f0de0ad41d
Zane Durkin <zanedurkin@gmail.com>
2018-02-15 23:19:49 -0800

    hw2 working

--- CS 240/hw2/msh.c
+++ CS 240/hw2/msh.c
@@ -11,15 +11,39 @@
 
 #define MAX_COMMAND 255
 
-void prompt(char * com[MAX_COMMAND][MAX_COMMAND]);// output '?:' and wait for response
+void prompt(char *(*command)[MAX_COMMAND] );// output '?:' and wait for response
 int main(){
 	pid_t childpid;
-	char *command[MAX_COMMAND];
+	char* command[MAX_COMMAND];
 	int com_len, child_ret_val;
-	while(1){
-		prompt(&command);
-		//printf("commmand is %s\n", com ); 
-		if( strcmp( command[0], "exit" ) == 0 ) return 0;
+	while( 1 ){
+		//prompt(&command);
+		//*
+		printf( "?: " );
+		int com_len = 0;
+		int com_arr = 0;
+		char c[MAX_COMMAND];
+		while( ( c[com_len] = getchar() ) != '\n' )	{		// read until end of line
+			if( c[com_len] == ' ' ){						// read until word ends
+
+				c[com_len] = '\0';							// close each word
+				command[com_arr] = malloc(com_len);			// create space for word in memory
+				strcpy(command[com_arr], c);				// copy string into array
+				com_arr = com_arr+1;						// increment words in array
+				com_len = 0;								// reset character counter
+				
+			}else{
+				com_len++;									// increment character counter
+			}
+		}
+		if( c[com_len] == ' ' || c[com_len] == '\n' )
+			c[com_len] = '\0';								// be sure last argument ends with terminator
+		command[com_arr] = c;								// copy over last argument
+		command[com_arr+1]= NULL;							// be sure to set last array element to null
+//*/
+		if( strcmp(command[0], "exit")==0 )					// exit if requested 
+			return 0;
+
 		if( ( childpid = fork() ) == 0 ){ // child function
 			printf("after fork command: %s\n",command[0]);
 			printf("arg1: %s\n", command[1]); 
@@ -42,31 +66,27 @@ int main(){
 
 }
 
-void prompt(char *com[MAX_COMMAND][MAX_COMMAND]){
+void prompt(char *(*command)[MAX_COMMAND]){
 	printf( "?: " );
 	int com_len = 0;
 	int com_arr = 0;
 	char c[MAX_COMMAND];
-	char cc;
-	int flag = 1;
-	while( ( cc = getchar() ) != '\n' ){
-		printf("char: %c\n", cc);
-		c[com_len] = (char)cc;
-		
-		if(c[com_len] == ' '){
-			flag = 1;
+	while( ( c[com_len] = getchar() ) != '\n' ){
+		if( c[com_len] == ' ' ){
 			c[com_len] = '\0';
-			*com[com_arr]= c;
+			*command[com_arr] = malloc(com_len);
+			strcpy(*command[com_arr], c);
 			com_arr = com_arr+1;
-			com_len = 0;
-			printf("part: %s\n", c);
+			com_len = 0;		
 		}else{
-			com_len++;
+			com_len++;									//
 		}
 	}
-	c[com_len] = '\0';
-	printf("part: %s\n", c);
-	*com[com_arr] = c;
-	printf("part: %s\n", *com[com_arr]);
-	//com[com_arr][com_len] = (char) NULL;
+	if( c[com_len] == ' ' || c[com_len] == '\n' )
+		c[com_len] = '\0';								// be sure last argument ends with terminator
+	*command[com_arr] = malloc(com_len);
+	strcpy(*command[com_arr], c);						// copy over last argument
+	*command[com_arr+1]= NULL;							// be sure to set last array element to null
+	printf("%s\n", c);
+	printf("%s\n", *command[com_arr]);
 }
